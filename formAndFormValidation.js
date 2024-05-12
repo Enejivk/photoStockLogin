e2517f670826b3d3afe80bf3edb1f202dbe6e6ea
enejivk <enejivic@gmail.com>
2024-05-12 17:27:40 +0100

    Adding comment to the form validation

--- formAndFormValidation.js
+++ formAndFormValidation.js
@@ -1,3 +1,5 @@
+/* GETTING ALL THE HTML ELEMENT I WILL BE NEEDING FOR THE VALIDATION */
+
 const confirmPasswordContainer = document.querySelector('.confirmPasswordContainer')
 const confirmPasswordInput = document.getElementById('confirm-password');
 const businessNameInput = document.getElementById('business-name');
@@ -23,6 +25,14 @@ for(let i = 0; i < passwordInputs.length; i++){
     })
 }
 
+
+/**
+ * This function checks length of an input value
+ * @param {string} input - this is the actual value of the input field
+ * @param {string} inputType - This is the input field that is calling the function
+ * @returns If there is any error it returns it
+ */
+
 const checkLengthInput = (input, inputType) => {
     if (input.length <= 10) {
         return `Your ${inputType} Should Be At Least 10 Characters.`
@@ -32,24 +42,51 @@ const checkLengthInput = (input, inputType) => {
     }
 }
 
-/* CHECKING FOR VALID NAME AND VALID BUSINESS NAME */
+
+/**
+ * It check for a valid name
+ * @param {string} str - String containing the name to be check
+ * @returns it return true it contains characters, numbers, and selected
+ * else it return false
+ */
+
 const isValidName = (str) => {
     const regex = /^[a-zA-Z0-9\s\-'.]+$/;
     return regex.test(str)
 }
 
-/* CHECKING FOR VALID WEBSITE */
+
+/**
+ * It uses regular expression to check if the string is a valid website
+ * @param {string} website - website to be checked
+ * @returns it returns true if is a valid websit
+ */
+
 const isValidWebsite = (website) => {
     const regex = /^(?!\.)(?:[a-z0-9\-]{1,63})(?:\.[a-z]{2,63})*?\.(com)$(?::\d{2,5})?(?:[/?#]\S*)?$/;
     return regex.test(website)
 }
 
-/* CHECKING FOR PASSWORD MATCH */
+
+
+/**
+ * checkPasswordMatch - check if password and confirm password match
+ * @param {string} password - password variable
+ * @param {string} confirmPassword - confirm password variable
+ * @returns It return true if is not match else false
+ */
 const checkPasswordMatch = (password, confirmPassword) => {
     if(password !== confirmPassword ){
         return true
     }
 }
+
+
+
+/**
+ * displayPasswordError - It display any password error
+ * @returns It return true if it display any error 
+ */
 
 const displayPasswordError = () => {
     /* removing the previous error message */
@@ -65,7 +102,12 @@ const displayPasswordError = () => {
     }
 }
 
-/* DISPLAY ERROR FOR WRONG WEBSITE */
+
+/**
+ * isPlayWebsiteError - It display any website name error
+ * @returns It return true if it display any error
+ */
+
 const displayWebsiteError = () => {
 
     /* removing the previous error message */
@@ -93,8 +135,14 @@ const displayBusinessError = () => {
     }
 }
 
+
+/**
+ * validateForm - validateForm call all the function and check if there is an error
+ * if there is error it stop the form from submitting else it submit the form and move to the next page
+ * @param {HTML} event - This is the element that triggered the event
+ */
+
 const validateForm = (event) => {
-    
     let isWebsiteError, isBusinessNameError, isConfirmPasswordError
     if(websiteInput.value.trim()){
         isWebsiteError = displayWebsiteError()
