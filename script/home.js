0846f1117f148b90176a8237c723a0354fd1d542
enejivk <enejivic@gmail.com>
2024-05-17 18:48:13 +0100

    working on the dashboard navigation

--- script/home.js
+++ script/home.js
@@ -1,7 +1,10 @@
 const button = document.querySelector('.toggle-nav')
 const outDashboard = document.querySelector('.outer-dashboard-container')
+const ul = document.querySelector('.dashboard-nav')
+
 
 button.addEventListener('click', () => {
     outDashboard.classList.toggle('hide-nav')
+    ul.style.display = 'none'
     
 })
