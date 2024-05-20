7eaefacb32d98a5c1caf07bae652bb59b950cf08
enejivk <enejivic@gmail.com>
2024-05-20 12:39:26 +0100

    updating graph

--- script/home.js
+++ script/home.js
@@ -12,54 +12,34 @@
 /* CODE FOR CREATING THE DEFAULT CHART */
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var gradient = ctx.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, 'rgba(0,0,0,1)');
-    gradient.addColorStop(0.59, 'rgba(157,157,157,0)');
-    gradient.addColorStop(1, 'rgba(254,254,254,1)');
 
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const data = {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
             datasets: [{
-                label: 'Sales',
-                data: [65, 59, 100, 0, 56, 38, 80],
-                fill: true,
-                backgroundColor: gradient,
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 3,
+                label: 'Dataset 1',
+                data: [1, 20, 15, 30, 10, 2, 30, 15, 40, 20],
+                borderColor: 'black',
+                borderWidth: 2,
+                fill: false,
             }]
-        },
-        options: {
-            responsive: true,
+        };
+
+        const options = {
+            responsive: false,
             maintainAspectRatio: false,
             scales: {
-                x: {
-                    grid: {
-                        display: false
-                    }
-                },
                 y: {
                     beginAtZero: true,
-                    grid: {
-                        display: false
-                    },
                     ticks: {
-                        stepSize: 5,
-                        callback: function(value, index, values) {
-                            return '$' + value;
-                        },
-                        color: 'rgba(0, 0, 0, 0.8)',
-                        font: {
-                            size: 14,
-                            family: 'Arial',
-                            weight: 'bold',
-                        }
+                        stepSize: 5
                     }
                 }
             }
-        }
-    });
-});
+        };
+
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: options
+        });
